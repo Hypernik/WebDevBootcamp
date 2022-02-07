d687f13aa3d9b08770fc1eb9e60893f42c58d62f
Nikhil Rawat <nikhil.rawat786.nr@gmail.com>
2022-02-07 23:41:30 +0530

    completed Newsletter App

---
+++ NodeJs/Newsletter-Signup/app.js
@@ -0,0 +1,61 @@
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.listen(process.env.PORT||3000,function () {
+    console.log("Server is running at port 3000");
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+mailchimp.setConfig({
+    apiKey: "e738f61374da543de2214ec277209315-us4",
+    server: "us4"
+});
+
+app.post("/", function (req,res) {
+    const firstName = req.body.firstName;
+    const secondName = req.body.secondName;
+    const email = req.body.email;
+
+    const listId = "3eaf194c2d";
+
+    const subscribingUser = {
+        firstName: firstName,
+        lastName: secondName,
+        email: email
+    };
+
+    async function run() {
+        const response = await mailchimp.lists.addListMember(listId, {
+            email_address: subscribingUser.email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: subscribingUser.firstName,
+                LNAME: subscribingUser.lastName
+            }
+        });
+
+        res.sendFile(__dirname + "/success.html")
+        console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`);
+    }
+    run().catch(e => res.sendFile(__dirname + "/failure.html"));
+});
+
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+});
+
+//Api Key
+//e738f61374da543de2214ec277209315-us4
+
+//List Id
+//3eaf194c2d
