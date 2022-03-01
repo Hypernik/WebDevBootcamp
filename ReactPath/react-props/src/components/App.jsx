3c779d3b1ab7f90d6d5ba6e80b9f1356ec4a35cb
Nikhil Rawat <nikhil.rawat786.nr@gmail.com>
2022-03-01 12:56:34 +0530

    complete React props

---
+++ ReactPath/react-props/src/components/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Contact from "./Contact";
+import contacts from "../contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Contact 
+        name={contacts[0].name}
+        imgUrl={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Contact 
+        name={contacts[1].name}
+        imgUrl={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Contact 
+        name={contacts[2].name}
+        imgUrl={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
